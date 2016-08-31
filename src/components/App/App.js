4e04d5885560fc80ac1fce5e27d13847a4206f9e
Michael Machiah <Michael@Michaels-MacBook-Pro.local>
2016-08-31 17:21:55 -0400

    saved

---
+++ src/components/App/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import Login from '../Login/Login';
+import Logout from '../Logout/Logout';
+import { userProfile, signIn, signOut } from '../../actions/authActions';
+
+class App extends Component {
+
+  get signIn() {
+    this.props.dispatch(userProfile());
+    this.props.dispatch(signIn());
+  }
+
+  get signOut() {
+    this.props.dispatch(signOut());
+  }
+
+  render() {
+    const { auth } = this.props;
+    if (!this.props.auth.logedIn) {
+      return (
+        <div>
+          <br/>
+          <br/>
+          <Login signIn={() => this.signIn} />
+          <br/>
+          <br/>
+          {this.props.children}
+        </div>
+      )
+    }
+    return (
+      <div>
+        <br/>
+        <br/>
+        <Login signIn={() => this.signIn} />
+        <Logout signOut={() => this.signOut} />
+        <ul>
+          <li>{auth.profile.fullname}</li>
+          <li>{auth.profile.email}</li>
+          <li><img src={auth.profile.profile_picture}/></li>
+        </ul>
+        <br/>
+        <br/>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state){
+  return ({ auth: state.auth });
+}
+
+export default connect(mapStateToProps)(App);
